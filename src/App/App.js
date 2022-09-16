15fa453653739a206ea138b759d34beb823c13eb
Nikita9901 <nikiticshsn2@gmail.com>
2022-09-16 11:33:06 +0300

    init: create and setup project

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import { Layout } from "../pages";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types";
+import styles from "./Layout.module.css";
+
+const Layout = () => {
+  return (
+    <>
+      <></>
+    </>
+  );
+};
+
+Layout.propTypes = {};
+
+export default Layout;
